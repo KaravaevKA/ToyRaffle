d47df9913f2479a909a54db38f2ed3ce60e46305
Kirill Karavaev <coffee.2016@yandex.ru>
2023-06-25 15:34:58 +0300

    Created class "Toy"

---
+++ src/base/Toy.java
@@ -0,0 +1,13 @@
+package base;
+
+public class Toy {
+    private int id;
+    private String name;
+    private int weight;
+
+    public Toy(int id, String name, int weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+}
